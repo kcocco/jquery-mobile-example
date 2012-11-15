4c06c905b95b4154cda345fa5c38ba9a19148b05
Kevin Cocco <kcocco@sproutloop.com>
2012-11-15 16:53:53 -0700

    Revert "Revert "test stripped down jqplot?""
    This reverts commit 00934f6cbf8bdd201f9eff11765724be68ea20bc.

--- www/js/main.js
+++ www/js/main.js
@@ -234,42 +234,7 @@ $('#clinic-display-graph').live('pageshow', function() {
      plot2b = $.jqplot('chart2b', [
      		[[7,'Tubal factor'], [7,'Ovulatory dysfunction'], [15,'Diminished ovarian reserve'],[4,'Endometriosis'],[1,'Uterine factor'],[17,'Male factor'],[7,'Other factor'],[12,'Unknown'],[11,'Multi Factors:Female Only'],[18,'Multi Factors:Female & Male']],
      		[[16,'Tubal factor'], [11,'Ovulatory dysfunction'], [23,'Diminished ovarian reserve'],[4,'Endometriosis'],[0,'Uterine factor'],[12,'Male factor'],[1,'Other factor'],[9,'Unknown'],[7,'Multi Factors:Female Only'],[18,'Multi Factors:Female & Male']]
- 		],{
-	     	title: {
-	     		text:'Patient Diagnosis',
-	     		textAlign:'left'
-	     	},
-	        seriesDefaults: {
-	            renderer:$.jqplot.BarRenderer,
-	            pointLabels: { show: true, location: 'e', edgeTolerance: -15 },
-	            shadow: false,
-	            shadowAngle: 135,
-	            rendererOptions: {barDirection: 'horizontal'}
-	        },
-	        series:[
-	        	{label:'National Avg.',lineWidth:20},
-	        	{label:'Selected Clinic',lineWidth:5}
-	        ],
-	        axes: {
-	        	xaxis: {
-	        		pad:1.18,
-	        		min:0,
-	        		label:'percentage %'
-	        	},
-	            yaxis: {
-	                renderer: $.jqplot.CategoryAxisRenderer
-	            }
-	        },
-	        legend: {
-		        show: true,
-		        location: 'n',     // compass direction, nw, n, ne, e, se, s, sw, w.
-		        placement: 'outside',
-		        //marginTop:
-		        //marginRight:
-		        //marginBottom:
-		        //marginLeft:
-		    }
-		});   	
+ 		]);   	
 });
 
 
